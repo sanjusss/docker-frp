53e4e8f387482653d103d7e2bb8e88303e5d48fd
sanjusss <sanjusss@qq.com>
2019-04-23 11:09:37 +0800

    修复构建错误

---
+++ build.sh
@@ -0,0 +1,11 @@
+﻿#/bin/bash
+set -e
+
+wget https://github.com/fatedier/frp/releases/download/v${APPVEYOR_REPO_TAG_NAME}/frp_${APPVEYOR_REPO_TAG_NAME}_linux_${FRP_ARCH}.tar.gz
+tar -xf ./frp_${APPVEYOR_REPO_TAG_NAME}_linux_${FRP_ARCH}.tar.gz
+mv ./frp_${APPVEYOR_REPO_TAG_NAME}_linux_${FRP_ARCH}/frpc ./frpc
+mv ./frp_${APPVEYOR_REPO_TAG_NAME}_linux_${FRP_ARCH}/frps ./frps
+
+os="linux"
+docker build --pull -t local_image:frpc-$os-$ARCH --build-arg "arch=$ARCH" ./frpc/
+docker build --pull -t local_image:frps-$os-$ARCH --build-arg "arch=$ARCH" ./frps/
